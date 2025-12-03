942fc330fecdffaafd2819b3eec747116d5dbf49
Florian Gillmann <mail@florian-gillmann.com>
2025-12-03 23:03:28 +0000

    feat: add day1 and helper functions

--- aoc/2025/day01/main.go
+++ aoc/2025/day01/main.go
@@ -1,8 +1,63 @@
 package main
 
-import "aoc/aoc/helpers"
+import (
+	"aoc/helpers"
+	"fmt"
+	"strings"
+)
 
 func main() {
 	data := helpers.ReadFile("input/2025/day01.txt")
-	_ = data
+
+	var rotations []string = strings.Split(data, "\n")
+	dial := 50
+	password1 := 0
+	password2 := 0
+
+	for i := range len(rotations) {
+		rotation := []rune(rotations[i])
+
+		if len(rotation) == 0 {
+			continue
+		}
+
+		direction := string(rotation[0])
+		distance := helpers.StringToInt(string(rotation[1:]))
+
+		dial, password1, password2 = turnDial(dial, direction, distance, password1, password2)
+	}
+
+	fmt.Printf("dial position: %s\n", fmt.Sprint(dial))
+	fmt.Printf("password1: %s\n", fmt.Sprint(password1))
+	fmt.Printf("password2: %s\n", fmt.Sprint(password2))
+}
+
+func turnDial(dial int, direction string, distance int, password1 int, password2 int) (int, int, int) {
+	var pos int
+
+	switch direction {
+	case "L":
+		pos = dial - distance
+
+		startFloor := (dial - 100) / 100
+		endFloor := (pos - 100) / 100
+		password2 += startFloor - endFloor
+	case "R":
+		pos = dial + distance
+		password2 += pos / 100
+	default:
+		panic("wtf")
+	}
+
+	pos = pos % 100
+
+	if pos < 0 {
+		pos += 100
+	}
+
+	if pos == 0 {
+		password1++
+	}
+
+	return pos, password1, password2
 }
--- helpers/helpers.go
+++ helpers/helpers.go
@@ -3,6 +3,7 @@ package helpers
 import (
 	"os"
 	"path/filepath"
+	"strconv"
 )
 
 func check(e error) {
@@ -15,4 +16,10 @@ func ReadFile(where string) string {
 	dat, err := os.ReadFile(filepath.Join(where))
 	check(err)
 	return string(dat)
+}
+
+func StringToInt(input string) int {
+	output, err := strconv.Atoi(input)
+	check(err)
+	return output
 }
